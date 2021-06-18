90dc8e056d4e8cd0ab96463d466b34adc835e8ed
roukorjerte <roukorjerte@gmail.com>
2021-06-18 20:45:03 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int firstNumber;
+	firstNumber = 5;
+	int secondNumber = 6;
+
+	int sum = firstNumber + secondNumber;
+	System.out.println (sum);
+
+	float firstFloat = 0.5f;
+	float secondFloat = 2.3f;
+
+	float floatSum = firstFloat + secondFloat;
+        System.out.println (floatSum);
+    }
+}
